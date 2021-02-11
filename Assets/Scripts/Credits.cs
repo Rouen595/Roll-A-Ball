519a033b6154aed9ca83dbb4a1769a3ec415d447
Rouen595 <76932810+Rouen595@users.noreply.github.com>
2021-02-11 16:13:21 -0600

    v2
    with updates

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -1,7 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
+    public void PlayAgain()
+    {
+        Debug.Log("Loading menu...");
+        Time.timeScale = 1f;
+        ChangeSpeed.gameSpeed = 1f;
+        PickColor.playerColor = 1;
+        GiantMode.giant = false;
+        SceneManager.LoadScene("Menu");
+    }
+
     public void Quit()
     {
         Debug.Log("QUIT");
